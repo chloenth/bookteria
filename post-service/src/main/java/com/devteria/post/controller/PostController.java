dcf7388c6a8d3727d956df03c31fcc7f27f7b37e
Huong <huongnguyen.nth1709@gmail.com>
2025-01-11 12:45:00 +0700

    implement pagination to post service

--- post-service/src/main/java/com/devteria/post/controller/PostController.java
+++ post-service/src/main/java/com/devteria/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.devteria.post.controller;
 
 import com.devteria.post.dto.ApiResponse;
+import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.service.PostService;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,19 +22,17 @@ import java.util.List;
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PostController {
-    PostService postService;
+	PostService postService;
 
-    @PostMapping("/create")
-    ApiResponse<PostResponse> createPost(@RequestBody PostRequest request){
-        return ApiResponse.<PostResponse>builder()
-                .result(postService.createPost(request))
-                .build();
-    }
+	@PostMapping("/create")
+	ApiResponse<PostResponse> createPost(@RequestBody PostRequest request) {
+		return ApiResponse.<PostResponse>builder().result(postService.createPost(request)).build();
+	}
 
-    @GetMapping("/my-posts")
-    ApiResponse<List<PostResponse>> myPosts(){
-        return ApiResponse.<List<PostResponse>>builder()
-                .result(postService.getMyPosts())
-                .build();
-    }
+	@GetMapping("/my-posts")
+	ApiResponse<PageResponse<PostResponse>> myPosts(
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+		return ApiResponse.<PageResponse<PostResponse>>builder().result(postService.getMyPosts(page, size)).build();
+	}
 }
--- post-service/src/main/java/com/devteria/post/repository/PostRepository.java
+++ post-service/src/main/java/com/devteria/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.devteria.post.repository;
 
-import com.devteria.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.devteria.post.entity.Post;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findAllByUserId(String userId);
+	Page<Post> findAllByUserId(String userId, Pageable pageable);
 }
